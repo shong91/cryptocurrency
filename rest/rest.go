49eca47031ec0caedc03f8b9c82545f916e8215a
shong91 <hhhongso@gmail.com>
2021-10-11 16:57:32 +0900

    Add jsonContentTypeMiddleWare

--- rest/rest.go
+++ rest/rest.go
@@ -67,7 +67,7 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 			Description: "See A Block",
 		},
 	}
-	rw.Header().Add("Content-Type", "application/json")
+	// rw.Header().Add("Content-Type", "application/json")
 
 	// Marshalling in order to send by JSON
 	// 소프트웨어는 바이트 단위로 데이터를 인식한다.메모리 바이트는 해석하는 틀에 따라 달라지는데, 이러한 변환(논리적 구조를 로우 바이트로 변환)을 '인코딩' 또는 '마샬링Mashaling'이라고 한다.
@@ -83,7 +83,7 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 func blocks(rw http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		rw.Header().Add("Content-Type", "application/json")
+		// rw.Header().Add("Content-Type", "application/json")
 		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
 	case "POST":
 		// decode requestbody
@@ -114,6 +114,14 @@ func block(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// create middleware
+func jsonContentTypeMiddleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request){
+		rw.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(rw, r)
+	})
+}
+
 func Start(aPort int) {
 	// ListenAndServe(port, nil): nil 일 경우 기본 multiplexer 를 사용
 	// multiplexer 는 클라이언트가 보낸 요청을 어디로 보낼지 결정하는데, main.go 에서 호출한 두 package의 Start() 가 같은 url 을 호출하고 있음 
@@ -123,6 +131,7 @@ func Start(aPort int) {
 	// use gorillaMux which is more effective than default multiplexer NewServerMux
 	router := mux.NewRouter()
 	port = fmt.Sprintf(":%d", aPort)
+	router.Use(jsonContentTypeMiddleWare)
 	router.HandleFunc("/", documentation).Methods("GET")
 	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
 	router.HandleFunc("/blocks/{height:[0-9]+}", block).Methods("GET") // gorillaMux can use regex
